cf34f17113656eb7bd7368fb83d545c36ee45162
brendanwang <brendanmwang@gmail.com>
2026-09-08 22:39:03 -0700

    Initial Commit

---
+++ USBConnection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  USBConnection
+//
+//  Created by Brendan Wang on 9/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ USBConnection/USBConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  USBConnectionApp.swift
+//  USBConnection
+//
+//  Created by Brendan Wang on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct USBConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
